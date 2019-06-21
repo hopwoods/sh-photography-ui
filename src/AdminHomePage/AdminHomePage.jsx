dc36aa8d19765ad3ede469ec3a2565c4ad9dc308
Stuart Hopwood <stoo.hopwood@gmail.com>
2019-06-21 09:32:19 +0100

    Tweaking font size. Sync between Navmenu & body

--- src/AdminHomePage/AdminHomePage.jsx
+++ src/AdminHomePage/AdminHomePage.jsx
@@ -1,56 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { userActions } from '../_actions';
+import { userActions } from "../_actions";
 
 class AdminHomePage extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(userActions.getAll());
-    }
+  componentDidMount() {
+    this.props.dispatch(userActions.getAll());
+  }
 
-    handleDeleteUser(id) {
-        return (e) => this.props.dispatch(userActions.delete(id));
-    }
+  handleDeleteUser(id) {
+    return e => this.props.dispatch(userActions.delete(id));
+  }
 
-    render() {
-        const { user, users } = this.props;
-        return (
-            <div className="col-md-6">
-                <h1>Hi {user.firstName}!</h1>
-                <p>You're logged in with React!!</p>
-                <h3>All registered users:</h3>
-                {users.loading && <em>Loading users...</em>}
-                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-                {users.items &&
-                    <ul>
-                        {users.items.map((user, index) =>
-                            <li key={user.id}>
-                                {user.firstName + ' ' + user.lastName}
-                                {
-                                    user.deleting ? <em> - Deleting...</em>
-                                    : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
-                                    : <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span>
-                                }
-                            </li>
-                        )}
-                    </ul>
-                }
-                <p>
-                    <Link to="/login">Logout</Link>
-                </p>
-            </div>
-        );
-    }
+  render() {
+    const { user, users } = this.props;
+    return (
+      <div className="col-md-6">
+        <h1>Hi {user.firstName}!</h1>
+        <p>You're logged in with React!!</p>
+        <h3>All registered users:</h3>
+        {users.loading && <em>Loading users...</em>}
+        {users.error && (
+          <span className="text-danger">ERROR: {users.error}</span>
+        )}
+        {users.items && (
+          <ul>
+            {users.items.map((user, index) => (
+              <li key={user.id}>
+                {user.firstName + " " + user.lastName}
+                {user.deleting ? (
+                  <em> - Deleting...</em>
+                ) : user.deleteError ? (
+                  <span className="text-danger">
+                    {" "}
+                    - ERROR: {user.deleteError}
+                  </span>
+                ) : (
+                  <span>
+                    {" "}
+                    - <a onClick={this.handleDeleteUser(user.id)}>Delete</a>
+                  </span>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
+        <p>
+          <Link to="/login">Logout</Link>
+        </p>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const { users, authentication } = state;
-    const { user } = authentication;
-    return {
-        user,
-        users
-    };
+  const { users, authentication } = state;
+  const { user } = authentication;
+  return {
+    user,
+    users
+  };
 }
 
 const connectedAdminHomePage = connect(mapStateToProps)(AdminHomePage);
